7c3fd83a3a89bb819ff0d1f9089bf8b678df5adf
Francis <smartfixosapp@gmail.com>
2026-04-01 18:44:16 -0400

    chore: auto push 2026-04-01 18:44:16

--- apps/Smart/src/components/ui/carousel.jsx
+++ apps/Smart/src/components/ui/carousel.jsx
@@ -1,11 +1,12 @@
 import * as React from "react"
+import { createContext } from "react"
 import useEmblaCarousel from "embla-carousel-react";
 import { ArrowLeft, ArrowRight } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 
-const CarouselContext = React.createContext(null)
+const CarouselContext = createContext(null)
 
 function useCarousel() {
   const context = React.useContext(CarouselContext)
