2b5c93f4d0061a33bf7e1197094033dadd515f18
Francis <smartfixosapp@gmail.com>
2026-04-09 00:01:44 -0400

    chore: auto push 2026-04-09 00:01:43

--- apps/Smart/src/pages/Financial.jsx
+++ apps/Smart/src/pages/Financial.jsx
@@ -33,6 +33,7 @@ import TransactionsModal from "../components/financial/TransactionsModal";
 import PurchaseOrderDialog from "../components/inventory/PurchaseOrderDialog";
 import PurchaseOrderDetailDialog from "../components/inventory/PurchaseOrderDetailDialog";
 import ImportPODialog from "../components/inventory/ImportPODialog";
+import FinancialCharts from "../components/financial/FinancialCharts";
 import { loadSuppliersSafe } from "@/components/utils/suppliers";
 import { useNavigate } from "react-router-dom";
 import { usePlanLimits } from "@/hooks/usePlanLimits";
